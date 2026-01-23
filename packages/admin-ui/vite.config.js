fca23a27ad8776d30c069361f704388a79410fa7
vaseapinkov <wasea.pinkov@gmail.com>
2026-01-23 06:22:14 +0200

    feat: improve library dx

--- packages/admin-ui/vite.config.js
+++ packages/admin-ui/vite.config.js
@@ -16,6 +16,7 @@ export default defineConfig({
             outDir: 'dist',
             staticImport: true,
             insertTypesEntry: true,
+            copyDtsFiles: true
         }),
     ],
     resolve: {
